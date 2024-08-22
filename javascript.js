54fc6db22009f120248cd2c96439677a833b8558
Guga2004-art <gustavocandidols2004@gmail.com>
2024-08-22 11:27:32 -0300

    Add files via upload
    Gerador de senha feito em Javascript
    Por estar em uma máquina mais antiga tive que postar assim mesmo

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const botao = document.getElementById("click")
+const texto = document.getElementById("senha")
+
+botao.addEventListener("click", () => {
+  const alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  const tamanhoAleatorio = Math.floor(Math.random() * 17) + 4
+  let senha = ''
+
+  for (let i = 0; i < tamanhoAleatorio; i++) {
+    const indiceAleatorio = Math.floor(Math.random() * alfabeto.length)
+    senha += alfabeto[indiceAleatorio]
+  }
+
+  texto.textContent = senha
+})
